ef367162a02fd701dadef66bd413a8f421120600
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-07-05 22:21:55 +0300

    DTOServicesImpl is done

--- app/src/main/java/kpn/lib/services/DTOServices.java
+++ app/src/main/java/kpn/lib/services/DTOServices.java
@@ -1,18 +1,13 @@
 package kpn.lib.services;
 
-import kpn.lib.deleter.DeleteServiceOld;
-import kpn.lib.domains.Domain;
-import kpn.lib.loader.LoadServiceOld;
-import kpn.lib.predicate.PredicateServiceOld;
+import kpn.lib.services.parts.deleting.service.DeletingService;
+import kpn.lib.services.parts.loading.service.LoadingService;
+import kpn.lib.services.parts.predicate.service.PredicateService;
 import kpn.lib.services.parts.saving.service.SavingService;
 
 public interface DTOServices<I, D, P, R> {
     SavingService<D, R> saver();
-
-
-    // TODO: !!!
-    // SaveService<I, D, R> saver();
-    // LoadService<I, R> loader();
-    // DeleteService<I, R> deleter();
-    // PredicateService<P, R> executor();
+    LoadingService<I, R> loader();
+    DeletingService<I, R> deleter();
+    PredicateService<P, R> executor();
 }
--- app/src/main/java/kpn/lib/services/DTOServicesImpl.java
+++ app/src/main/java/kpn/lib/services/DTOServicesImpl.java
@@ -2,60 +2,93 @@ package kpn.lib.services;
 
 import java.util.function.Function;
 
-import kpn.lib.deleter.DeleteServiceOld;
-import kpn.lib.domains.Domain;
-import kpn.lib.loader.LoadServiceOld;
-import kpn.lib.predicate.PredicateServiceOld;
+import kpn.lib.services.parts.deleting.service.DeletingDecorator;
+import kpn.lib.services.parts.deleting.service.DeletingService;
+import kpn.lib.services.parts.loading.service.LoadingDecorator;
+import kpn.lib.services.parts.loading.service.LoadingService;
+import kpn.lib.services.parts.predicate.service.PredicateDecorator;
+import kpn.lib.services.parts.predicate.service.PredicateService;
+import kpn.lib.services.parts.saving.service.SavingDecorator;
 import kpn.lib.services.parts.saving.service.SavingService;
-import kpn.lib.services.result.ImmutableServiceResult;
 import kpn.lib.services.result.ServiceResult;
 
 public class DTOServicesImpl<I, D, P, R> implements DTOServices<I, D, P, R> {
     private final SavingService<D, ServiceResult<D>> saver;
+    private final LoadingService<I, ServiceResult<D>> loader;
+    private final DeletingService<I, ServiceResult<D>> deleter;
+    private final PredicateService<P, ServiceResult<D>> executor;
     private final Function<ServiceResult<D>, R> converter;
 
-    public DTOServicesImpl(SavingService<D, ServiceResult<D>> saver, Function<ServiceResult<D>, R> converter) {
+    public static <I, D, P, R> Builder<I, D, P, R> buider(Function<ServiceResult<D>, R> converter){
+        return new Builder<>(converter);
+    }
+
+    private DTOServicesImpl(SavingService<D, ServiceResult<D>> saver, 
+                            LoadingService<I, ServiceResult<D>> loader,
+                            DeletingService<I, ServiceResult<D>> deleter,
+                            PredicateService<P, ServiceResult<D>> executor,
+                            Function<ServiceResult<D>, R> converter) {
         this.saver = saver;
+        this.loader = loader;
+        this.deleter = deleter;
+        this.executor = executor;
         this.converter = converter;
     }
 
     @Override
     public SavingService<D, R> saver() {
-        // TODO Auto-generated method stub
-        return null;
+        return new SavingDecorator<>(saver, converter);
     }
 
-    // TODO: del
-    // TODO: private ???
-    // public static class SavingDecorator<D, R> implements SavingService<D, R>{
-    //     private final SavingService<D, ServiceResult<D>> saver;
-    //     private final Function<ServiceResult<D>, R> converter;
+    @Override
+    public LoadingService<I, R> loader() {
+        return new LoadingDecorator<>(loader, converter);
+    }
 
-    //     public SavingDecorator(SavingService<D, ServiceResult<D>> saver,
-    //                            Function<ServiceResult<D>, R> converter) {
-    //         this.saver = saver;
-    //         this.converter = converter;
-    //     }
+    @Override
+    public DeletingService<I, R> deleter() {
+        return new DeletingDecorator<>(deleter, converter);
+    }
 
-    //     @Override
-    //     public R save(D domain) {
-    //         return null;
-    //     }
-    // }
+    @Override
+    public PredicateService<P, R> executor() {
+        return new PredicateDecorator<>(executor, converter);
+    }
 
-    // private static class SaverDecorator<D> implements SavingService<D>{
-    //     private final SavingService<D> saver;
+    public static class Builder<I, D, P, R>{
+        private final Function<ServiceResult<D>, R> converter;
 
-    //     public SaverDecorator(SavingService<D> saver) {
-    //         this.saver = saver;
-    //     }
+        private SavingService<D, ServiceResult<D>> saver;
+        private LoadingService<I, ServiceResult<D>> loader;
+        private DeletingService<I, ServiceResult<D>> deleter;
+        private PredicateService<P, ServiceResult<D>> executor;
 
-    //     // TODO: must ret R
-    //     @Override
-    //     public ServiceResult<D> save(D domain) {
-    //         return saver != null
-    //             ? saver.save(domain)
-    //             : new ImmutableServiceResult<>("service.saving.absent");
-    //     }
-    // }
+        public Builder(Function<ServiceResult<D>, R> converter) {
+            this.converter = converter;
+        }
+
+        public Builder<I,D,P,R> saver(SavingService<D, ServiceResult<D>> saver){
+            this.saver = saver;
+            return this;
+        }
+
+        public Builder<I,D,P,R> loader(LoadingService<I, ServiceResult<D>> loader){
+            this.loader = loader;
+            return this;
+        }
+
+        public Builder<I,D,P,R> deleter(DeletingService<I, ServiceResult<D>> deleter){
+            this.deleter = deleter;
+            return this;
+        }
+
+        public Builder<I,D,P,R> executor(PredicateService<P, ServiceResult<D>> executor){
+            this.executor = executor;
+            return this;
+        }
+
+        public DTOServices<I, D, P, R> build(){
+            return new DTOServicesImpl<>(saver, loader, deleter, executor, converter);
+        }
+    }
 }
